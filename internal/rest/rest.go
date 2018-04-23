eadcb2ff6d77a43639b1ab9eec6526880b569f1a
Steven Motes <steven.motes@senseye.io>
2018-04-23 18:05:05 -0500

    added docs

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -8,11 +8,15 @@ import (
 	"net/url"
 )
 
+// Client represents a generic HTTP REST client that handles
+// a JSON structure in requests and responses.
 type Client struct {
 	root *url.URL
 	cli  *http.Client
 }
 
+// NewClient returns a new instance of *Client from the provided
+// inner *http.Client cli and API base *url.URL root.
 func NewClient(cli *http.Client, root *url.URL) *Client {
 	return &Client{
 		cli:  cli,
@@ -20,6 +24,10 @@ func NewClient(cli *http.Client, root *url.URL) *Client {
 	}
 }
 
+// BuildRequest builds the specified *http.Request value from the
+// provided request method, path, body and optional body/query
+// parameters, with the appropriate headers set depending on
+// the particular request method.
 func (cli *Client) BuildRequest(method, path string, body io.Reader, q url.Values) (*http.Request, error) {
 	u := *cli.root
 	u.Path = path
@@ -39,6 +47,7 @@ func (cli *Client) BuildRequest(method, path string, body io.Reader, q url.Value
 	return req, nil
 }
 
+// Error represents the structure of an error received from the mountebank API.
 type Error struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
@@ -48,6 +57,10 @@ func (err Error) Error() string {
 	return fmt.Sprintf("%s: %s", err.Code, err.Message)
 }
 
+// ProcessRequest processes the provided *http.Request value req and
+// decodes the response body JSON into the value pointed to by v if
+// the response code matches the provided code, or decodes into an
+// error otherwise.
 func (cli *Client) ProcessRequest(req *http.Request, code int, v interface{}) error {
 	resp, err := cli.cli.Do(req)
 	if err != nil {
