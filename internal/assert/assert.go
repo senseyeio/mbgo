92531bf3d94ef3ec81e898d34d02552eabfa0b4a
Steven Motes <steven.motes@gmail.com>
2019-03-31 18:05:30 -0500

    refactor testutil into assert package

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,4 +1,4 @@
-package testutil
+package assert
 
 import (
 	"fmt"
@@ -8,9 +8,9 @@ import (
 	"testing"
 )
 
-// ExpectEqual is a helper function used throughout the unit and integration
+// Equals is a helper function used throughout the unit and integration
 // tests to assert deep equality between an actual and expected value.
-func ExpectEqual(tb testing.TB, actual, expected interface{}) {
+func Equals(tb testing.TB, actual, expected interface{}) {
 	if !reflect.DeepEqual(actual, expected) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, expected, actual)
