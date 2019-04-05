378d3beaba737f3f69edff853e514d8256619950
Steven Motes <steven.motes@gmail.com>
2019-04-05 12:04:14 -0500

    update assert.Equals to be a test helper, remove colors in assertion failure output

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,19 +1,16 @@
 package assert
 
 import (
-	"fmt"
-	"path/filepath"
 	"reflect"
-	"runtime"
 	"testing"
 )
 
 // Equals is a helper function used throughout the unit and integration
 // tests to assert deep equality between an actual and expected value.
 func Equals(tb testing.TB, actual, expected interface{}) {
-	if !reflect.DeepEqual(actual, expected) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, expected, actual)
-		tb.FailNow()
+	tb.Helper()
+
+	if !reflect.DeepEqual(expected, actual) {
+		tb.Fatalf("\n\n\texpected: %#v\n\n\tactual: %#v\n\n", expected, actual)
 	}
 }
