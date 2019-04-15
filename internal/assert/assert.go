041cb30e307549a030c5d30eafb17ab6bd1faba8
Steven Motes <steven.motes@gmail.com>
2019-04-15 00:55:50 -0500

    added err ok function to assertion package

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -14,3 +14,12 @@ func Equals(tb testing.TB, actual, expected interface{}) {
 		tb.Fatalf("\n\n\texpected: %#v\n\n\tactual: %#v\n\n", expected, actual)
 	}
 }
+
+// Ok fails the test if an err is not nil.
+func Ok(tb testing.TB, err error) {
+	tb.Helper()
+
+	if err != nil {
+		tb.Fatalf("unexpected error: %#v\n\n", err)
+	}
+}
