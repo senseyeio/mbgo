088a4b85209ebb05751d6f6480c382890d3d7f40
Steven Motes <steven.motes@gmail.com>
2022-06-20 13:38:46 -0500

    general cleanup

--- client.go
+++ client.go
@@ -1,7 +1,6 @@
 // Copyright (c) 2018 Senseye Ltd. All rights reserved.
 // Use of this source code is governed by the MIT License that can be found in the LICENSE file.
 
-// Package mbgo implements a mountebank API client.
 package mbgo
 
 import (
